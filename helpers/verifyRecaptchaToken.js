863d3f1f280c30ed515c49e65330693b83bb38b9
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-19 17:56:38 +0100

    51

--- helpers/verifyRecaptchaToken.js
+++ helpers/verifyRecaptchaToken.js
@@ -1,33 +1,25 @@
-const axios = require('axios');
+const axios = require('axios')
+
+
 
 const verifyRecaptchaToken = async (token, ip) => {
-    try {
-        const params = new URLSearchParams();
-        params.append('secret', '6LfKH_spAAAAAOXNkwc2hCeJzapZ7LlJ5W_2h2zJ');
-        params.append('response', token);
-        if (ip) params.append('remoteip', ip); // Optionally include the user's IP address
-
-        const googleResponse = await axios.post('https://www.google.com/recaptcha/api/siteverify', params.toString(), {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        });
-
+    try{
+        const googleResponse= await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`)
         console.log(googleResponse.data);
         const { success } = googleResponse.data;
-
         if (success) {
-            const response = { message: 'Form submitted successfully', success: true };
-            return response;
+            const response = { message: 'Form submitted successfully', success: true}
+            return response
         } else {
-            const response = { message: 'reCAPTCHA verification failed', success: false, code: 400 };
-            return response;
+            const response = { message: 'reCAPTCHA verification failed', success: false, code: 400}
+            return response
         }
-    } catch (error) {
-        console.log(error);
-        const response = { message: 'Internal server error', success: false, code: 500 };
-        return response;
     }
-};
+    catch(error){
+        console.log(error);
+        const response = { message: 'Internal server error', success: false, code: 500}
+        return response
+    }
+}
 
-module.exports = verifyRecaptchaToken;
+module.exports = verifyRecaptchaToken
