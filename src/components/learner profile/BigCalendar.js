646605dee231919b50d8fe20fc97d5ba3578091d
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-29 23:59:39 +0100

    66

--- src/components/learner profile/BigCalendar.js
+++ src/components/learner profile/BigCalendar.js
@@ -1,88 +1,111 @@
 import { useState, useRef, useEffect } from 'react';
-import { IoIosArrowDroprightCircle, IoIosArrowDropleftCircle } from "react-icons/io";
-import { useDispatch } from 'react-redux';
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import { useDispatch} from 'react-redux';
 import { setFirstLessonList } from '../../state/slices/lessonsList';
 import GenerateCalendarGrid from './GenerateCalendarGrid';
-import axiosInstance from '../../interceptors/axiosInterceptor';
+import axiosInstance from '../../interceptors/axiosInterceptor'
 import { useLocation } from 'react-router-dom';
 import Footer from '../Global/Footer';
+
 
 function BigCalendar() {
     const [year, setYear] = useState(new Date().getFullYear());
     const [month, setMonth] = useState(new Date().getMonth());
-    const [slideDirection, setSlideDirection] = useState(null);
+    const [slideDirection, setSlideDirection] = useState(null)
     const location = useLocation();
-    const dispatch = useDispatch();
-    const calendarGridRef = useRef(null);
 
     const path = window.location.pathname;
-    const segments = path.split('/');
-    const firstSegment = segments[1];
 
+    // Split the path by "/"
+    const segments = path.split('/');
+
+    // Get the value of the first segment
+    const firstSegment = segments[1]; 
+    
+
+    const dispatch = useDispatch()
+
+    const calendarGridRef = useRef(null);
+
+    //handling the animation when sliding 
     useEffect(() => {
         if (slideDirection) {
+            // Add animation class based on the slide direction
             calendarGridRef.current.classList.add(`slide-${slideDirection}`);
+            // Remove animation class after animation ends
             const handleAnimationEnd = () => {
                 calendarGridRef.current.classList.remove(`slide-${slideDirection}`);
                 setSlideDirection(null);
             };
             calendarGridRef.current.addEventListener('animationend', handleAnimationEnd);
+            // Clean up event listener
             return () => {
                 calendarGridRef.current.removeEventListener('animationend', handleAnimationEnd);
             };
         }
     }, [slideDirection]);
 
+
     const months = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
 
+    
+
+    //control the next button
     const prevMonth = () => {
         if (month === 0) {
             setYear(year - 1);
-            setMonth(11);
+            setMonth(11); // December
         } else {
             setMonth(month - 1);
         }
         setSlideDirection("right");
     };
 
+    //contorl the next button
     const nextMonth = () => {
         if (month === 11) {
             setYear(year + 1);
-            setMonth(0);
+            setMonth(0); // January
         } else {
             setMonth(month + 1);
         }
         setSlideDirection("left");
     };
 
-    const actualDay = () => {
-        setYear(new Date().getFullYear());
-        setMonth(new Date().getMonth());
-    };
+    //button to get back to actual date
+    const actualDay =() => {
+        setYear(new Date().getFullYear())
+        setMonth(new Date().getMonth())
+    }
 
+    //getting the first lesson in every day
     const fetchData = async () => {
         try {
-            const response = await axiosInstance.post(`https://onlinelearningplatform-d9w2.onrender.com/${firstSegment}/getFirstLesson`, {}, {
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('accesstoken')}`
-                }
-            });
-            console.log('API Response:', response.data.message);
-            dispatch(setFirstLessonList(response.data.message));
-        } catch (err) {
-            console.log(err);
+           const response = await axiosInstance.post(`https://onlinelearningplatform-d9w2.onrender.com/${firstSegment}/getFirstLesson`, {}, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('accesstoken')}`
+            }
+        })
+        console.log('API Response:', response.data.message);
+        dispatch(setFirstLessonList(response.data.message))
+        }catch(err) {
+            console.log(err)
         }
-    };
+    }
+
 
     useEffect(() => {
-        fetchData();
-    }, [dispatch, firstSegment]);
+        fetchData()
+    }, [])
+    
+
 
     return (
-        <div className={`bg-backg overflow-y-auto ${location.pathname.startsWith('/learner/profile/Tutor/') ? "p-3 h-full" : "py-4 px-4 lg:px-36 h-[90%] "} overflow-x-hidden rounded-lg shadow-lg`}>
+        <div className={`bg-backg overflow-y-auto ${location.pathname.startsWith('/learner/profile/Tutor/')? "p-3 h-full" : "py-4 px-4 lg:px-36 h-[90%] "} overflow-x-hidden rounded-lg shadow-lg`}>
             <div className="calendar-header relative w-full flex items-center lg:justify-center mb-4">
                 <div className="flex items-center space-x-7 lg:space-x-36">
                     <IoIosArrowDropleftCircle size="25" className="text-button hover:text-orange-600 cursor-pointer" onClick={prevMonth}></IoIosArrowDropleftCircle>
@@ -92,12 +115,21 @@ function BigCalendar() {
                 <button onClick={actualDay} className="absolute right-4 cursor-pointer px-4 py-2 bg-lightbutton hover:bg-orange-600 hover:text-white hover:border-none border rounded-lg text-button border-button">Today</button>
             </div>
             <div ref={calendarGridRef} className={`calendar-grid grid grid-cols-7 gap-2`}>
-                {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => (
-                    <div key={day} className="rounded-lg bg-lightg py-2 text-center text-black font-bold ">{day}</div>
-                ))}
+                <div className="rounded-lg bg-lightg py-2 text-center text-black font-bold ">Sun</div>
+                <div className="rounded-lg bg-lightg py-2 text-center text-black font-bold ">Mon</div>
+                <div className="rounded-lg bg-lightg py-2 text-center text-black font-bold ">Tue</div>
+                <div className="rounded-lg bg-lightg py-2 text-center text-black font-bold ">Wed</div>
+                <div className="rounded-lg bg-lightg py-2 text-center text-black font-bold ">Thu</div>
+                <div className="rounded-lg bg-lightg py-2 text-center text-black font-bold ">Fri</div>
+                <div className="rounded-lg bg-lightg py-2 text-center text-black font-bold ">Sat</div>
                 <GenerateCalendarGrid month={month} year={year}></GenerateCalendarGrid>
             </div>
-            {["/learner/profile/Calendar", "/tutor/profile/Calendar"].includes(path) && <Footer />}
+            {
+                path ==="/learner/profile/Calendar" || path === "/tutor/profile/Calendar"?
+                <Footer></Footer>
+                :
+                null
+            }
         </div>
     );
 }
--- src/components/learner profile/CalendarSchedule/ThridStep.js
+++ src/components/learner profile/CalendarSchedule/ThridStep.js
@@ -1,77 +1,71 @@
-
-import { MdNavigateBefore } from "react-icons/md"
-import {useDispatch, useSelector} from 'react-redux'
-import { IoMdCalendar } from "react-icons/io"
-import {useState, useEffect} from 'react'
-import axiosInstance from "../../../interceptors/axiosInterceptor"
-import { setTutorSearchList } from "../../../state/slices/userSlice"
-import Loading from "../../Global/Loading"
-import TutorRow from "./TutorRow"
-import { convertTime } from "../../Global/functions"
-
-
+import { MdNavigateBefore } from "react-icons/md";
+import { useDispatch, useSelector } from 'react-redux';
+import { IoMdCalendar } from "react-icons/io";
+import { useState, useEffect } from 'react';
+import axiosInstance from "../../../interceptors/axiosInterceptor";
+import { setTutorSearchList } from "../../../state/slices/userSlice";
+import Loading from "../../Global/Loading";
+import TutorRow from "./TutorRow";
+import { convertTime } from "../../Global/functions";
 
 function ThirdStep(props) {   
-    const dispatch = useDispatch()
-    const learnerData = useSelector(state => state.userData)
-    const scheduleData = useSelector(state => state.scheduleData )
+    const dispatch = useDispatch();
+    const learnerData = useSelector(state => state.userData);
+    const scheduleData = useSelector(state => state.scheduleData);
 
-
-    const [tutorName, setTutorName] = useState("")
-
-    const [loading, setLoading] = useState(false)
-
-    const [filteredList, setFilteredTutors] = useState(learnerData.tutorSearchList)
-
+    const [tutorName, setTutorName] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [filteredList, setFilteredTutors] = useState(learnerData.tutorSearchList);
 
     //getting the tutors from database
     async function fetchData () {
         try {
-                setLoading(true)
-                const {formattedHours, formattedMinutes} = convertTime(scheduleData.time)
+            setLoading(true);
+            const { formattedHours, formattedMinutes } = convertTime(scheduleData.time);
             
-                // Construct the normal time string
-                const normalTime = `${formattedHours}:${formattedMinutes}`
+            // Construct the normal time string
+            const normalTime = `${formattedHours}:${formattedMinutes}`;
+            const selectedDate = `${scheduleData.selectedDate} ${normalTime}`;
     
-                console.log("normalTime: ", normalTime)
+            console.log("normalTime: ", normalTime);
 
-                const selectedDate = scheduleData.selectedDate + " "+ normalTime
-                const response = await axiosInstance.post('https://onlinelearningplatform-d9w2.onrender.com/learner/getFreeTutors', {
-                    selectedDate: selectedDate,
-                    lessonLength: scheduleData.lessonLength,
-                    lessonTopic: scheduleData.lessonTopic, 
-                    Language: scheduleData.language
-                }, {
-                    headers: {
-                        'Authorization': `Bearer ${localStorage.getItem('accesstoken')}`
-                    }
-                });
-                dispatch(setTutorSearchList(response.data.message))
-                setFilteredTutors(response.data.message)
-                setLoading(false)
-            }catch(err) {
-                console.log(err)
-                setLoading(false)
-            }
+            const response = await axiosInstance.post('https://onlinelearningplatform-d9w2.onrender.com/learner/getFreeTutors', {
+                selectedDate: selectedDate,
+                lessonLength: scheduleData.lessonLength,
+                lessonTopic: scheduleData.lessonTopic, 
+                Language: scheduleData.language
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('accesstoken')}`
+                }
+            });
+            dispatch(setTutorSearchList(response.data.message));
+            setFilteredTutors(response.data.message);
+            setLoading(false);
+        } catch (err) {
+            console.log(err);
+            setLoading(false);
+        }
     }
 
     //this function only fires when the list is empty 
     useEffect(() => {
-        fetchData()
-    }, [])
-
+        if (learnerData.tutorSearchList.length === 0) {
+            fetchData();
+        }
+    }, []);
 
     //taking name input from user and comparing the input with the list elements in lower case to avoid case sensitivity 
     const handleSearching = (e) => {
         setTutorName(e.target.value);
-        const inputName = e.target.value.toLowerCase()
+        const inputName = e.target.value.toLowerCase();
 
         const filteredList = learnerData.tutorSearchList.filter(tutor => {
-            if(tutor.firstname && tutor.lastname) {
+            if (tutor.firstname && tutor.lastname) {
                 const fullName = `${tutor.firstname.toLowerCase()} ${tutor.lastname.toLowerCase()}`;
                 return fullName.includes(inputName) || tutor.firstname.toLowerCase().includes(inputName) || tutor.lastname.toLowerCase().includes(inputName);
-            }else {
-                return true
+            } else {
+                return true;
             }
         });
         setFilteredTutors(filteredList); 
@@ -79,8 +73,6 @@ function ThirdStep(props) {
         console.log("tutor: ", filteredList);
     }
 
-
-    
     return (
         <>
             <div className="relative flex justify-center items-center w-full">
@@ -92,31 +84,26 @@ function ThirdStep(props) {
                 <span className="text-active">{props.selectedDate}</span>
             </div>
             {
-                learnerData.tutorSearchList.length>0?
+                learnerData.tutorSearchList.length > 0 &&
                 <input
-                type="search"
-                value={tutorName}
-                onChange={handleSearching}
-                placeholder="Search for tutor..."
-                className="w-full mb-5 px-4 py-2 border rounded-xl focus:outline-none focus:border-elements transition-colors duration-300"
+                    type="search"
+                    value={tutorName}
+                    onChange={handleSearching}
+                    placeholder="Search for tutor..."
+                    className="w-full mb-5 px-4 py-2 border rounded-xl focus:outline-none focus:border-elements transition-colors duration-300"
                 />
-                :
-                null
             }
-            <div className={`${learnerData.tutorSearchList.length>0? 'overflow-y-auto ' : ''} py-4 pr-5 w-full flex flex-col space-y-4`}>
+            <div className={`${learnerData.tutorSearchList.length > 0 ? 'overflow-y-auto ' : ''} py-4 pr-5 w-full flex flex-col space-y-4`}>
                 {
-                    loading?
-                    <Loading></Loading>
-                    :
+                    loading ?
+                    <Loading /> :
                     (
-                        filteredList.length>0?
-                        filteredList.map((tutor, index) => {
-                            return <TutorRow key={index} moveForward={props.moveForward} tutor={tutor}></TutorRow>
-                        })
-                        :
-                        <img src="/No-Data.png" alt="no data" className="flex justify-center items-center w-80 h-80 object-cover" ></img>
+                        filteredList.length > 0 ?
+                        filteredList.map((tutor, index) => (
+                            <TutorRow key={index} moveForward={props.moveForward} tutor={tutor} />
+                        )) :
+                        <img src="/No-Data.png" alt="no data" className="flex justify-center items-center w-80 h-80 object-cover" />
                     )
-
                 }
             </div>
         </>
