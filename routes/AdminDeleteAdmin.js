0897d6f01312729ad7589f102f01611955ab898d
Yassine Hamza <yassine.hamza@esprit.tn>
2024-07-04 00:31:20 +0100

    117

--- routes/AdminDeleteAdmin.js
+++ routes/AdminDeleteAdmin.js
@@ -1,25 +1,25 @@
-// const express = require('express');
-// const router = express.Router();
-// const mysql = require('../helpers/Sql_connection');
-// const auth = require('../middleware/auth');
-// const roleCheck = require('../middleware/roleCheck');
+const express = require('express');
+const router = express.Router();
+const mysql = require('../helpers/Sql_connection');
+const auth = require('../middleware/auth');
+const roleCheck = require('../middleware/roleCheck');
 
 
-// router.post('/DeleteAdminAccount', auth, roleCheck(["Admin"]),(req, res) => {
-//     const { userId } = req.body;
+router.post('/DeleteAdminAccount', auth, roleCheck(["Admin"]),(req, res) => {
+    const { userId } = req.body;
 
     
      
-//     const query = `DELETE FROM administrator WHERE id = ?`;
+    const query = `DELETE FROM administrator WHERE id = ?`;
 
-//     mysql.query(query, [userId], (err, result) => {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).json({ message: "Internal Server Error" });
-//         } else {
-//             res.status(204).json({ message: "Deleted" });
-//         }
-//     });
-// });
+    mysql.query(query, [userId], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ message: "Internal Server Error" });
+        } else {
+            res.status(204).json({ message: "Deleted" });
+        }
+    });
+});
 
-// module.exports = router;
+module.exports = router;
