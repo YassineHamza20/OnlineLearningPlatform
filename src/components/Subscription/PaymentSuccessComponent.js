419d80294249a7992885c2d18c2ca5c6a93a2039
Yassine Hamza <yassine.hamza@esprit.tn>
2024-08-08 02:02:56 +0100

    commit

--- src/components/Subscription/PaymentSuccessComponent.js
+++ src/components/Subscription/PaymentSuccessComponent.js
@@ -23,7 +23,7 @@ const PaymentSuccessComponent = () => {
             console.log(`Payment success for learnerId: ${learnerId}, subId: ${subId}, paymentId: ${paymentId}, duration: ${duration}`);
 
             // Make the API call to update the backend
-            axios.get('/learner/payment_success', {
+            axios.get('https://onlinelearningplatform-d9w2.onrender.com/learner/payment_success', {
                 params: {
                     learnerId,
                     subId,
